3a7d5613fb58381ea26c002e8d3b67120479115a
JinJeongBeom <95035130+JinJeongBeom@users.noreply.github.com>
2022-03-31 02:39:59 +0900

    create activity_main.xml

--- app/src/main/java/com/hansung/android/androidproject1/MainActivity.java
+++ app/src/main/java/com/hansung/android/androidproject1/MainActivity.java
@@ -3,6 +3,8 @@ package com.hansung.android.androidproject1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
